ee3b20fd70d781b7abd4347ad5ee14179340bebd
Roman Medvedev <r.medvedev@parad-shoes.ru>
2021-02-14 00:03:11 +0300

    separated functions. renamed variables

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const http = require('http');
 const url = require('url');
-/*
-const express = require('express');
-const app = express();
-app.use("/list", function (request, response) {
 
-    response.send("<h1>list</h1>");
-});
-app.listen(5000);
-*/
 http.createServer(function (req, res) {
     let urlParts = url.parse(req.url);
     console.log(urlParts);
@@ -25,20 +17,6 @@ http.createServer(function (req, res) {
         } else {
             page404(req, res);
         }
-        /*
-        switch (urlParts.pathname) {
-            case "/":
-                homepage(req, res);
-                break;
-            case "/list":
-            case "/list/":
-                list(req, res, urlParts);
-                break;
-            default:
-                page404(req, res);
-                break;
-        }
-        */
     }
     else if (req.method == 'POST') {
         switch (urlParts.pathname) {
@@ -57,32 +35,28 @@ http.createServer(function (req, res) {
 }).listen(3000);
 console.log("Start");
 
+let arr = ['text for page 1', 'text for page 2', 'text for page 3', 'text for page 4', 'text for page 5'];
+
 function homepage(req, res) {
     res.end("homepage");
 }
+
 function list(req, res, urlParts) {
-    let arr = ['text for page 1', 'text for page 2', 'text for page 3', 'text for page 4', 'text for page 5'];
     let datasend = {};
     let query = {};
-    let page = 0;
+    let currentPage = 0;
     let pageSize = 1;
-    let totalpage = arr.length;
+    let totalPage = arr.length;
     if (urlParts.query) {
-        urlParts.query.split('&')
-            .forEach((one) => {
-                let part = one.split('=');
-                if (part.length === 2 && part[1] !== '') {
-                    return (query[part[0]] = part[1])
-                }
-            });
+        query = parseQuery(urlParts.query);
     }
     if (query && query.page) {
-        page = parseInt(query.page, 10);
-        if (!(page >= 0)) {
-            page = 0;
+        currentPage = parseInt(query.page, 10);
+        if (!(currentPage >= 0)) {
+            currentPage = 0;
         }
-        if ((page >= totalpage)) {
-            page = totalpage - 1;
+        if ((currentPage >= totalPage)) {
+            currentPage = totalPage - 1;
         }
     }
     if (query && query.pageSize) {
@@ -90,23 +64,48 @@ function list(req, res, urlParts) {
         if (!(pageSize > 0)) {
             pageSize = 1;
         }
-        if (pageSize > Math.ceil(totalpage / (page + 1))) {
+        if (pageSize > Math.ceil(totalPage / (currentPage + 1))) {
             pageSize = 1;
         }
     }
     query.pageSize = pageSize;
-    query.page = page;
-    datasend.text = (arr.slice(page, (page + 1) * pageSize));
+    query.page = currentPage;
+    datasend.text = (arr.slice(currentPage, (currentPage + 1) * pageSize));
     datasend.settings = query;
-    datasend.settings.totalpage = totalpage;
+    datasend.settings.totalPage = totalPage;
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(datasend));
     //res.end();
 }
+
+function testPageSize(queryPageSize, totalPage, currentPage) {
+    let localPageSize = parseInt(queryPageSize, 10);
+    if (!(localPageSize > 0)) {
+        localPageSize = 1;
+    }
+    if (localPageSize > Math.ceil(totalPage / (currentPage + 1))) {
+        localPageSize = 1;
+    }
+    return localPageSize;
+}
+
+function parseQuery(stringquery) {
+    let resultquery = {};
+    stringquery.split('&')
+        .forEach((one) => {
+            let part = one.split('=');
+            if (part.length === 2 && part[1] !== '') {
+                return (resultquery[part[0]] = part[1])
+            }
+        });
+    return resultquery;
+}
+
 function createitem(req, res) {
     res.end("created");
 }
+
 function page404(req, res) {
     res.end("404");
 }
